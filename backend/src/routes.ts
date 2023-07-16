30bc0732a8d300c47e7097d03f3d807899f11f8d
Giovana Santana Barbosa <giovanastbarbosa@gmail.com>
2023-07-16 16:27:24 -0300

    Listagem de produtos por categoria

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -12,6 +12,7 @@ import { CreateCategoryController } from './controllers/category/CreateCategoryC
 import { ListCategoryController } from './controllers/category/ListCategoryController';
 
 import { CreateProductController } from './controllers/product/CreateProductController';
+import { ListByCategoryController } from './controllers/product/ListByCategoryController';
 
 const router = Router();
 
@@ -28,5 +29,6 @@ router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
 //-- ROTAS PRODUCT --
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
+router.get('/category/product', isAuthenticated, new ListByCategoryController().handle)
 
 export { router }; 
