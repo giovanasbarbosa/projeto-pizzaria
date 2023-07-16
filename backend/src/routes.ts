adf2162cc386deba0886e5b7306a8768cabbbe13
Giovana Santana Barbosa <giovanastbarbosa@gmail.com>
2023-07-16 17:11:57 -0300

    Deletando um pedido

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -14,6 +14,10 @@ import { ListCategoryController } from './controllers/category/ListCategoryContr
 import { CreateProductController } from './controllers/product/CreateProductController';
 import { ListByCategoryController } from './controllers/product/ListByCategoryController';
 
+import { CreateOrderController } from './controllers/order/CreateOrderController';
+import { RemoveOrderController } from './controllers/order/RemoveOrderController';
+
+
 const router = Router();
 
 const upload = multer(uploadConfig.upload("./tmp"))
@@ -30,5 +34,9 @@ router.get('/category', isAuthenticated, new ListCategoryController().handle)
 //-- ROTAS PRODUCT --
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
 router.get('/category/product', isAuthenticated, new ListByCategoryController().handle)
+
+//-- ROTAS ORDER --
+router.post('/order', isAuthenticated, new CreateOrderController().handle)
+router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 
 export { router }; 
