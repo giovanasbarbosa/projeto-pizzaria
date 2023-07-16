18f5995c9cd157e030890dda536fe119686cfb1d
Giovana Santana Barbosa <giovanastbarbosa@gmail.com>
2023-07-16 19:08:57 -0300

    Adição e remoção de ítens de um pedido

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -16,6 +16,8 @@ import { ListByCategoryController } from './controllers/product/ListByCategoryCo
 
 import { CreateOrderController } from './controllers/order/CreateOrderController';
 import { RemoveOrderController } from './controllers/order/RemoveOrderController';
+import { AddItemController } from './controllers/order/AddItemController';
+import { RemoveItemController } from './controllers/order/RemoveItemController';
 
 
 const router = Router();
@@ -38,5 +40,7 @@ router.get('/category/product', isAuthenticated, new ListByCategoryController().
 //-- ROTAS ORDER --
 router.post('/order', isAuthenticated, new CreateOrderController().handle)
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
+router.post('/order/add', isAuthenticated, new AddItemController().handle)
+router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
 
 export { router }; 
