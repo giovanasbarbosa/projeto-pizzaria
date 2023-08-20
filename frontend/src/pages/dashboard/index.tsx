1d7acd148e27aee20755661e1b4f4caa32cec0d8
Giovana Santana Barbosa <giovanastbarbosa@gmail.com>
2023-08-20 16:09:37 -0300

    Modal e finalização do dashboard

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -3,8 +3,84 @@ import Head from "next/head"
 import { Header } from "../../components/Header"
 import styles from './styles.module.scss'
 import { FiRefreshCcw } from 'react-icons/fi'
+import {setupAPIClient} from '../../services/api'
+import { useState } from "react"
+import Modal from 'react-modal'
+import {ModalOrder} from '../../components/ModalOrder'
 
-export default function Dashboard(){
+type OrderProps={
+    id: string;
+    table: string | number;
+    status: boolean;
+    draft: boolean;
+    name: string | null;
+}
+
+interface HomeProps{
+    orders: OrderProps[];
+}
+
+export type OrderItemProps={
+    id: string;
+    amount: number;
+    order_id: string;
+    product_id: string;
+    product:{
+        id: string;
+        name: string;
+        description: string;
+        price: string;
+        banner: string;
+    }
+    order:{
+        id: string;
+        table: string | number;
+        status: boolean;
+        name: string | null;
+    }
+}
+
+export default function Dashboard({orders}: HomeProps){
+   const [orderList, setOrderList] = useState(orders||'')
+   const [modalItem, setModalItem] = useState<OrderItemProps[]>()
+   const [modalVisible, setModalVisible] = useState(false)
+
+function handleCloseModal(){
+    setModalVisible(false)
+}
+
+   async function handleOpenModalView(id: string){
+        const apiClient = setupAPIClient()
+        const response = await apiClient.get('/order/detail', {
+          params:{
+            order_id: id,
+          }  
+        })
+
+        setModalItem(response.data)
+        setModalVisible(true)
+   }
+
+   async function handleFinishItem(id: string){
+    const apiClient = setupAPIClient()
+    await apiClient.put('/order/finish', {
+        order_id: id,
+    })
+
+    const response = await apiClient.get('/orders')
+    setOrderList(response.data)
+
+    setModalVisible(false)
+   }
+
+   async function handleRefreshOrders(){
+    const apiClient = setupAPIClient()
+    const response = await apiClient.get('/orders')
+    setOrderList(response.data)
+   }
+
+   Modal.setAppElement('#__next')
+
     return(
         <>
         <Head>
@@ -19,31 +95,54 @@ export default function Dashboard(){
 
                     <div className={styles.containerHeader}>
                         <h1>Últimos pedidos</h1>
-                        <button>
+                        <button onClick={handleRefreshOrders}>
                             <FiRefreshCcw size={25} color="#3fffa3"/>
                         </button>
                     </div>
 
                     <article className={styles.listOrders}>
 
-                        <section className={styles.orderItem}>
-                            <button>
+                        {orderList.length === 0 && (
+                            <span className={styles.emptylist}>
+                                Nenhum pedido aberto foi encontrado.
+                            </span>
+                        )}
+
+                        {orderList.map(item => (
+                            <section key={item.id} className={styles.orderItem}>
+                            <button onClick={()=>handleOpenModalView(item.id)}>
                                 <div className={styles.tag}></div>
-                                <span>Mesa 30</span>
+                                <span>Mesa {item.table}</span>
                             </button>
-                        </section>
+                            </section>
+                        ))}
 
                     </article>
 
                 </main>
+
+                {modalVisible && (
+                    <ModalOrder
+                    isOpen={modalVisible}
+                    onRequestClose={handleCloseModal}
+                    order={modalItem}
+                    handleFinishOrder={handleFinishItem}
+                    />
+                )} 
 
             </div>
         </ >
     )
 }
 
-export const getServerSideProps = canSSRAuth(async (contexto) => {
+export const getServerSideProps = canSSRAuth(async (contexto) => { 
+    const apiClient = setupAPIClient(contexto)
+    const response = await apiClient.get('/orders')
+    //console.log(response.data)
+
     return{
-        props:{}
+        props:{
+            orders: response.data
+        }
     }
 })
